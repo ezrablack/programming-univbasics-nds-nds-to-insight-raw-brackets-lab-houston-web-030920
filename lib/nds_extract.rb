5b64ec9108e5600997395fa9e77f0b38db43cd1e
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-28 01:11:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,18 +1,22 @@
-require 'pp' 
-
+require 'pp'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-def directors_totals(nds)
+ def directors_totals(nds)
   all_directors = nds
   
   grand_totals = {}
+  
+  total_movie_gross = 0
   directors = 0 
-  while directors < all_directors.count do
-    total_movie_gross = 0 
+  while directors < all_directors.length do
     movie_counter = 0 
-    while movie_counter < all_directors[directors][:movies].count do 
-      total_movie_gross += all_directors[directors][:movies][movie_counter][:worldwide_gross]
+    while movie_counter < all_directors[directors].length do 
+      title = 0 
+      while title < all_directors[directors][:movies].length do 
+        total_movie_gross += all_directors[directors][movie_counter][title][:worldwide_gross] += 1 
+        title += 1 
+      end
       movie_counter += 1 
     end
     grand_totals[all_directors[directors][:name]] = total_movie_gross
@@ -21,12 +25,5 @@ def directors_totals(nds)
   return grand_totals
 end
 
-#def print_first_directors_movie_titles
-#  movies = directors_database[0][:movies]
-	
-#	movie_index = 0
-#	while movie_index < movies.count do
-#		puts "#{movies[movie_index][:title]}"
-#		movie_index += 1
-#	end
-#end
+
+
--- lib/nds_extract_dup.rb
+++ lib/nds_extract_dup.rb
@@ -1,12 +1,13 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
+#$LOAD_PATH.unshift(File.dirname(__FILE__))
+#require 'directors_database'
 
-def directors_totals(nds)
+#def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  
   result = {
   }
   #
@@ -18,5 +19,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+#  nil
+#end
